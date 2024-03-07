99377acdcb2ec94cfc73b238fbc4f9c538472ccb
Eddie Reyes <108855058+eddie-reyes@users.noreply.github.com>
2024-03-06 17:08:46 -0800

    Delete client directory

--- client/shaders/fragment.glsl
+++
@@ -1,8 +0,0 @@
-varying vec3 vertexNormal;
-uniform vec3 glowColor;
-uniform float c;
-
-void main() {
-    float intensity = pow(0.8 - dot(vertexNormal, vec3(0, 0, 1.0)), 2.0);
-    gl_FragColor = vec4(glowColor, c) * intensity;
-}
